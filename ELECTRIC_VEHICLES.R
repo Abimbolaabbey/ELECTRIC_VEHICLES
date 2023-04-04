2f980097b7de05d2851ea735cd728142c0b074f7
Abimbola Abiodun John <67701907+Surckerg@users.noreply.github.com>
2023-04-04 16:01:56 +0100

    Add files via upload

--- ELECTRIC_VEHICLES.R
+++ ELECTRIC_VEHICLES.R
@@ -3,25 +3,33 @@ install.packages()
 library(readr)
 library(ggplot2)
 
-Electric_Vehicles_2 <- read_csv("C:/Users/maxla/Downloads/Electric_Vehicles_2.csv")
+#Imported my dataset
 
-View(Electric_Vehicles_2)
+      Electric_Vehicles_2 <- read_csv("C:/Users/maxla/Downloads/Electric_Vehicles_2.csv")
+
+      #View the dataset
+      
+          View(Electric_Vehicles_2)
+          
 
 #Correlation between recharge time and range
 
-Correlation<-cor(Electric_Vehicles_2$`Recharge Time (h)`,Electric_Vehicles_2$`Range (km)`)
+         Correlation<-cor(Electric_Vehicles_2$`Recharge Time (h)`,Electric_Vehicles_2$`Range (km)`)
                  
                  
 #Correlation between Battery capacity and range
 
-Correlation_KW<-cor(Electric_Vehicles_2$Battery_capacity_in_KW,Electric_Vehicles_2$`Range (km)`)
+           Correlation_KW<-cor(Electric_Vehicles_2$Battery_capacity_in_KW,Electric_Vehicles_2$`Range (km)`)
                     
-                    #Correlation between recharge time and range
+                    
+#Scatter plot of the Correlation between recharge time and range
                     
                       
                       plot(Electric_Vehicles_2$`Recharge Time (h)`,Electric_Vehicles_2$`Range (km)`,col= "blue",main = "Correlation Between Recharge Time and Range",xlab = "Recharge Time (h)", ylab = "Range (km)")
                       
+                     
                       
+ #Scatter plot of the Correlation between battery capacity and range
                       
                       plot(Electric_Vehicles_2$Battery_capacity_in_KW,Electric_Vehicles_2$`Range (km)`,col= "red",main = "Correlation Between Battery capacity and Range",xlab = "Battery Capacity(KW)", ylab = "Range (km)")
                       
